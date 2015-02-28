6f4355a68bf580f367b87f79346ba606da0c8438
Stevan Little <stevan.little@gmail.com>
2015-02-28 22:09:41 +0100

    move everything back to multis

--- test.pl
+++ test.pl
@@ -11,122 +11,150 @@ class Env {
 
 ## ---------------------------------------------
 
-class Ast {
+class Ast {}
 
-    method evaluate ( Env $env ) {
-        die "Unknown Ast Node: " ~ self;
-    }
-}
-
-class Const is Ast {
+class Literal is Ast {
     has Any $.value;
-
-    method evaluate ( Env $env ) {
-        return self;
-    }
-
-    method Str { "Const(" ~ $.value ~ ")" }
 }
 
-class Var is Ast {
+class Variable is Ast {
     has Str $.name;
-
-    method evaluate ( Env $env ) {
-        return $env.get( $.name ) // die "Unable to find the variable: " ~ $.name;
-    }
-
-    method Str { "Var\{" ~ $.name ~ "\}" }
 }
 
-class Cons is Ast {
+class ConsCell is Ast {
     has Ast $.head;
     has Ast $.tail;
-
-    method evaluate ( Env $env ) {
-        return self;
-    }    
- 
-    method Str { "Cons[ " ~ $.head ~ " :: " ~ $.tail ~ " ]" }
 }
 
-class Fun is Ast {
+class Function is Ast {
     has     @.params;
     has Ast $.body;
-
-    method evaluate ( Env $env ) {
-        return -> @args {
-            my $new_env = $env.clone;
-            loop (my $i = 0; $i < @args.elems; $i++ ) {
-                $new_env.set( @.params[ $i ], @args[ $i ] )
-            }
-            $.body.evaluate( $new_env );
-        }
-    }
-
-    method Str { "Fun (" ~ @.params.join(", ") ~ ") \{ " ~ $.body ~ " \}" }
 }
 
 class Let is Ast {
     has Str $.name;
     has Ast $.value;
     has Ast $.body;
+}
 
-    method evaluate ( Env $env ) {
-        my $new_env = $env.clone;
-        $new_env.set( $.name, $.value.evaluate( $env ) );
-        return $.body.evaluate( $new_env );
-    }
+class LetRec is Ast {
+    has     @.definitions;
+    has Ast $.body;
+}
 
-    method Str { "Let " ~ $.name ~ " = " ~ $.value ~ "; " ~ $.body ~ ";" }
+class Condition is Ast { 
+    has Ast $.condition;
+    has Ast $.if_true;
+    has Ast $.if_false;
 }
 
 class Apply is Ast {
     has Str $.name;
     has     @.args;
-
-    method evaluate ( Env $env ) {
-
-        my $code    = $env.get( $.name ) // die "Unable to find function to apply: " ~ $.name;;
-        my $new_env = $env.clone;
-        $code(
-            self.args.map( -> $arg { 
-                $arg.evaluate( $new_env ) 
-            })
-        );
-    }
-
-    method Str { "Appy " ~ $.name ~ "(" ~ @.args.join(", ") ~ ")" }
 }
+
+## ---------------------------------------------
+
+multi pprint ( Ast       $node ) { "" }
+multi pprint ( Literal   $node ) { "Literal(" ~ $node.value ~ ")" }
+multi pprint ( Variable  $node ) { "Variable(" ~ $node.name ~ ")" }
+multi pprint ( ConsCell  $node ) { "Cons[ " ~ $node.head ~ " !! " ~ $node.tail ~ " ]" }
+multi pprint ( Function  $node ) { "Fun (" ~ $node.params.join(", ") ~ ") \{ " ~ $node.body ~ " \}" }
+multi pprint ( Let       $node ) { "Let " ~ $node.name ~ " = " ~ $node.value ~ "; " ~ $node.body ~ ";" }
+multi pprint ( LetRec    $node ) { "Let Rec " ~ $node.definitions ~ "; " ~ $node.body ~ ";" }
+multi pprint ( Apply     $node ) { "Appy " ~ $node.name ~ "(" ~ $node.args.join(", ") ~ ")" }
+multi pprint ( Condition $node ) { "Condition " ~ $node.condition ~ "{" ~ $node.if_true ~ "} else {" ~ $node.if_false ~ "}" }
+
+## ---------------------------------------------
+
+multi evaluate ( Ast $exp, Env $env ) {
+    die "Unknown Ast Node: " ~ $exp;
+}
+
+multi evaluate ( Literal $exp, Env $env ) {
+    return $exp;
+}
+
+multi evaluate ( Variable $exp, Env $env ) {
+    return $env.get( $exp.name ) // die "Unable to find the variable: " ~ $exp.name;
+}
+
+multi evaluate ( ConsCell $exp, Env $env ) {
+    return $exp;
+}    
+
+multi evaluate ( Function $exp, Env $env ) {
+    return -> @args {
+        my $new_env = $env.clone;
+        loop (my $i = 0; $i < @args.elems; $i++ ) {
+            $new_env.set( $exp.params[ $i ], @args[ $i ] )
+        }
+        evaluate( $exp.body, $new_env );
+    }
+}
+
+multi evaluate ( Let $exp, Env $env ) {
+    my $new_env = $env.clone;
+    $new_env.set( $exp.name, evaluate( $exp.value, $env ) );
+    return evaluate( $exp.body, $new_env );
+}
+
+multi evaluate ( LetRec $exp, Env $env ) {
+    
+}
+
+multi evaluate ( Apply $exp, Env $env ) {
+    my $code    = $env.get( $exp.name ) // die "Unable to find function to apply: " ~ $exp.name;
+    my $new_env = $env.clone;
+    $code(
+        $exp.args.map( -> $arg { evaluate( $arg, $new_env ) })
+    );
+}
+
+multi evaluate ( Condition $exp, Env $env ) {
+    evaluate( $exp.condition, $env ).value == $env.get('#TRUE').value
+        ?? evaluate( $exp.if_true, $env )
+        !! evaluate( $exp.if_false, $env )
+}
+
+## ---------------------------------------------
+
+my $TRUE  = Literal.new( value => 1 );
+my $FALSE = Literal.new( value => 0 );
 
 ## ---------------------------------------------
 
 my Env $root_env = Env.new;
 
-$root_env.set(
-    '+', -> ($l, $r) { 
-        Const.new( value => $l.value + $r.value )
-    }
-);
+$root_env.set('#TRUE',  $TRUE);
+$root_env.set('#FALSE', $FALSE);
 
-say ~ Let.new(
-        name  => 'add',
-        value => Fun.new(
-            params => [ 'x', 'y' ],
-            body   => Apply.new(
-                name => '+',
-                args => [
-                    Var.new( name => 'x' ),
-                    Var.new( name => 'y' ),
-                ]
-            )
-        ),
-        body  => Apply.new(
-            name => 'add',
+$root_env.set('+', -> ($l, $r) { Literal.new( value => $l.value + $r.value ) });
+$root_env.set('*', -> ($l, $r) { Literal.new( value => $l.value * $r.value ) });
+$root_env.set('/', -> ($l, $r) { Literal.new( value => $l.value / $r.value ) });
+$root_env.set('-', -> ($l, $r) { Literal.new( value => $l.value - $r.value ) });
+
+$root_env.set('==', -> ($l, $r) { ($l.value == $r.value) ?? $TRUE !! $FALSE });
+$root_env.set('!=', -> ($l, $r) { ($l.value != $r.value) ?? $TRUE !! $FALSE });
+$root_env.set('<' , -> ($l, $r) { ($l.value <  $r.value) ?? $TRUE !! $FALSE });
+$root_env.set('<=', -> ($l, $r) { ($l.value <= $r.value) ?? $TRUE !! $FALSE });
+$root_env.set('>' , -> ($l, $r) { ($l.value >  $r.value) ?? $TRUE !! $FALSE });
+$root_env.set('>=', -> ($l, $r) { ($l.value >= $r.value) ?? $TRUE !! $FALSE });
+
+say pprint evaluate(
+    Condition.new(
+        condition => Apply.new(
+            name => '==',
             args => [
-                Const.new( value => 20 ),
-                Const.new( value => 30 ),
+                Literal.new( value => 2 ),
+                Literal.new( value => 2 ),
             ]
         ),
-    ); #.evaluate( $root_env );
+        if_true  => Literal.new( value => 'TRUE!!!'  ),
+        if_false => Literal.new( value => 'FALSE!!!' ),
+
+    ), 
+    $root_env 
+);
 
 ## ---------------------------------------------
