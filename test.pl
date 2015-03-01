698e1ea4382ce2e8d5c4de7bfc7d519c59595c90
Stevan Little <stevan.little@gmail.com>
2015-03-01 22:38:42 +0100

    whole bunch of tweaks

--- test.pl
+++ test.pl
@@ -2,22 +2,13 @@
 
 ## ---------------------------------------------
 
-class Env {
-    has %.pad;
-
-    method get    ( Str $key )             { %.pad{ $key }          }
-    method set    ( Str $key, Any $value ) { %.pad{ $key } = $value }
-}
-
-## ---------------------------------------------
-
 class Ast {}
 
 class Literal is Ast {
     has Any $.value;
 }
 
-class Variable is Ast {
+class Var is Ast {
     has Str $.name;
 }
 
@@ -26,9 +17,14 @@ class ConsCell is Ast {
     has Ast $.tail;
 }
 
-class Function is Ast {
-    has     @.params;
+class Func is Ast {
+    has Str @.params;
     has Ast $.body;
+}
+
+class NativeFunc is Ast {
+    has Str   @.params;
+    has Block $.extern;
 }
 
 class Let is Ast {
@@ -38,11 +34,11 @@ class Let is Ast {
 }
 
 class LetRec is Ast {
-    has     @.definitions;
-    has Ast $.body;
+    has Pair @.definitions;
+    has Ast  $.body;
 }
 
-class Condition is Ast { 
+class Cond is Ast { 
     has Ast $.condition;
     has Ast $.if_true;
     has Ast $.if_false;
@@ -50,161 +46,185 @@ class Condition is Ast {
 
 class Apply is Ast {
     has Str $.name;
-    has     @.args;
+    has Ast @.args;
 }
 
 ## ---------------------------------------------
 
-multi pprint ( Ast       $node ) { "" }
-multi pprint ( Literal   $node ) { "Literal(" ~ $node.value ~ ")" }
-multi pprint ( Variable  $node ) { "Variable(" ~ $node.name ~ ")" }
-multi pprint ( ConsCell  $node ) { "Cons[ " ~ $node.head ~ " !! " ~ $node.tail ~ " ]" }
-multi pprint ( Function  $node ) { "Fun (" ~ $node.params.join(", ") ~ ") \{ " ~ $node.body ~ " \}" }
-multi pprint ( Let       $node ) { "Let " ~ $node.name ~ " = " ~ $node.value ~ "; " ~ $node.body ~ ";" }
-multi pprint ( LetRec    $node ) { "Let Rec " ~ $node.definitions ~ "; " ~ $node.body ~ ";" }
-multi pprint ( Apply     $node ) { "Appy " ~ $node.name ~ "(" ~ $node.args.join(", ") ~ ")" }
-multi pprint ( Condition $node ) { "Condition " ~ $node.condition ~ "{" ~ $node.if_true ~ "} else {" ~ $node.if_false ~ "}" }
+class Env {
+    has Env $.parent;
+    has Ast %.pad;
+
+    method get ( Str  $key  ) { %.pad{ $key } // $.parent.?get( $key ) }
+    method set ( Pair $pair ) { %.pad{ $pair.key } = $pair.value       }
+}
+
+class Scope {
+    has Env @.env;
+
+    method current { @.env[*-1] }
+    method enter   { @.env.push( Env.new( :parent( self.current ) ) ) }
+    method leave   { @.env.pop }
+}
 
 ## ---------------------------------------------
 
-multi evaluate ( Ast $exp, Env $env ) {
+multi evaluate ( Ast $exp, Scope $scope ) {
     die "Unknown Ast Node: " ~ $exp;
 }
 
-multi evaluate ( Literal $exp, Env $env ) {
+multi evaluate ( Literal $exp, Scope $scope ) {
     return $exp;
 }
 
-multi evaluate ( Variable $exp, Env $env ) {
-    return $env.get( $exp.name ) // die "Unable to find the variable: " ~ $exp.name;
+multi evaluate ( Var $exp, Scope $scope ) {
+    return $scope.current.get( $exp.name ) // die "Unable to find the variable: " ~ $exp.name;
 }
 
-multi evaluate ( ConsCell $exp, Env $env ) {
+multi evaluate ( ConsCell $exp, Scope $scope ) {
     return $exp;
 }    
 
-multi evaluate ( Function $exp, Env $env ) {
-    return -> @args {
-        my $new_env = $env.clone;
-        loop (my $i = 0; $i < @args.elems; $i++ ) {
-            $new_env.set( $exp.params[ $i ], @args[ $i ] )
-        }
-        evaluate( $exp.body, $new_env );
+multi evaluate ( Let $exp, Scope $scope ) {
+    $scope.enter;
+    $scope.current.set: $exp.name => evaluate( $exp.value, $scope );
+    my $result = evaluate( $exp.body, $scope );
+    $scope.leave;
+    $result;
+}
+
+multi evaluate ( LetRec $exp, Scope $scope ) {
+    $scope.enter;
+    for $exp.definitions -> $def { 
+        $scope.current.set: $def.key => evaluate( $def.value, $scope ) 
     }
+    my $result = evaluate( $exp.body, $scope );
+    $scope.leave;
+    $result;
 }
 
-multi evaluate ( Let $exp, Env $env ) {
-    my $new_env = $env.clone;
-    $new_env.set( $exp.name, evaluate( $exp.value, $env ) );
-    return evaluate( $exp.body, $new_env );
+multi evaluate ( Func $exp, Scope $scope ) {
+    return $exp;
 }
 
-multi evaluate ( LetRec $exp, Env $env ) {
-    my $new_env = $env.clone;
-    my @defs    = $exp.definitions.clone;
+multi evaluate ( NativeFunc $exp, Scope $scope ) {
+    return $exp;
+}
 
-    while ( @defs ) {
-        my ($var, $value) = (@defs.shift, @defs.shift);
-        $env.set( $var, evaluate( $value, $new_env ) );
+multi evaluate ( Apply $exp, Scope $scope ) {
+    my $code = $scope.current.get( $exp.name ) // die "Unable to find function to apply: " ~ $exp.name;
+
+    $scope.enter;
+
+    loop (my $i = 0; $i < $exp.args.elems; $i++ ) {
+        $scope.current.set: $code.params[ $i ] => evaluate( $exp.args[ $i ], $scope );
     }
 
-    return evaluate( $exp.body, $new_env );
+    my $result = do if $code.?extern {
+        $code.extern.( $scope );
+    } else {
+        evaluate( $code.body, $scope );
+    };
+
+    $scope.leave;
+    $result;
 }
 
-multi evaluate ( Apply $exp, Env $env ) {
-    my $code    = $env.get( $exp.name ) // die "Unable to find function to apply: " ~ $exp.name;
-    my $new_env = $env.clone;
-    $code(
-        $exp.args.map( -> $arg { evaluate( $arg, $new_env ) })
-    );
-}
-
-multi evaluate ( Condition $exp, Env $env ) {
-    evaluate( $exp.condition, $env ) === $env.get('#TRUE')
-        ?? evaluate( $exp.if_true, $env )
-        !! evaluate( $exp.if_false, $env )
+multi evaluate ( Cond $exp, Scope $scope ) {
+    evaluate( $exp.condition, $scope ) === $scope.current.get('#TRUE')
+        ?? evaluate( $exp.if_true, $scope )
+        !! evaluate( $exp.if_false, $scope )
 }
 
 ## ---------------------------------------------
 
-my $TRUE  = Literal.new( value => 1 );
-my $FALSE = Literal.new( value => 0 );
+my $TRUE  = Literal.new( :value( True  ) );
+my $FALSE = Literal.new( :value( False ) );
 
 ## ---------------------------------------------
 
-my Env $root_env = Env.new;
+my Env $root_env .= new;
 
-$root_env.set('#TRUE',  $TRUE);
-$root_env.set('#FALSE', $FALSE);
+$root_env.set: '#TRUE'  => $TRUE;
+$root_env.set: '#FALSE' => $FALSE;
 
-$root_env.set('+', -> ($l, $r) { Literal.new( value => $l.value + $r.value ) });
-$root_env.set('*', -> ($l, $r) { Literal.new( value => $l.value * $r.value ) });
-$root_env.set('/', -> ($l, $r) { Literal.new( value => $l.value / $r.value ) });
-$root_env.set('-', -> ($l, $r) { Literal.new( value => $l.value - $r.value ) });
+$root_env.set: '+' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { Literal.new( :value( $scope.current.get('l').value + $scope.current.get('r').value ) ) } ) );
+$root_env.set: '*' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { Literal.new( :value( $scope.current.get('l').value * $scope.current.get('r').value ) ) } ) );
+$root_env.set: '/' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { Literal.new( :value( $scope.current.get('l').value / $scope.current.get('r').value ) ) } ) );
+$root_env.set: '-' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { Literal.new( :value( $scope.current.get('l').value - $scope.current.get('r').value ) ) } ) );
 
-$root_env.set('==', -> ($l, $r) { ($l.value == $r.value) ?? $TRUE !! $FALSE });
-$root_env.set('!=', -> ($l, $r) { ($l.value != $r.value) ?? $TRUE !! $FALSE });
-$root_env.set('<' , -> ($l, $r) { ($l.value <  $r.value) ?? $TRUE !! $FALSE });
-$root_env.set('<=', -> ($l, $r) { ($l.value <= $r.value) ?? $TRUE !! $FALSE });
-$root_env.set('>' , -> ($l, $r) { ($l.value >  $r.value) ?? $TRUE !! $FALSE });
-$root_env.set('>=', -> ($l, $r) { ($l.value >= $r.value) ?? $TRUE !! $FALSE });
+$root_env.set: '==' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { ($scope.current.get('l').value == $scope.current.get('r').value) ?? $TRUE !! $FALSE } ) );
+$root_env.set: '!=' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { ($scope.current.get('l').value != $scope.current.get('r').value) ?? $TRUE !! $FALSE } ) );
+$root_env.set: '<'  => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { ($scope.current.get('l').value <  $scope.current.get('r').value) ?? $TRUE !! $FALSE } ) );
+$root_env.set: '<=' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { ($scope.current.get('l').value <= $scope.current.get('r').value) ?? $TRUE !! $FALSE } ) );
+$root_env.set: '>'  => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { ($scope.current.get('l').value >  $scope.current.get('r').value) ?? $TRUE !! $FALSE } ) );
+$root_env.set: '>=' => NativeFunc.new( :params( 'l', 'r' ) :extern( sub ($scope) { ($scope.current.get('l').value >= $scope.current.get('r').value) ?? $TRUE !! $FALSE } ) );
 
-say pprint evaluate
-    LetRec.new(
-        definitions => [
-            'mul', Function.new(
-                params => [ 'x', 'y' ],
-                body   => Condition.new(
-                    condition => Apply.new( 
-                        name => '==',
-                        args => [
-                            Variable.new( name => 'y' ),
-                            Literal.new( value => 1 )
-                        ]
-                    ),
-                    if_true  => Variable.new( name => 'x' ),
-                    if_false => Apply.new(
-                        name => '+',
-                        args => [
-                            Variable.new( name => 'x' ),
-                            Apply.new(
-                                name => 'mul',
-                                args => [
-                                    Variable.new( name => 'x' ),
-                                    Apply.new(
-                                        name => '-',
-                                        args => [
-                                            Variable.new( name => 'y' ),
-                                            Literal.new( value => 1 )
-                                        ]
-                                    )
-                                ]
+my $root_node = LetRec.new(
+    :definitions(
+        'mul' => Func.new(
+            :params( 'x', 'y' ),
+            :body(
+                Cond.new(
+                    :condition(
+                        Apply.new( 
+                            :name( '==' ),
+                            :args(
+                                Var.new( :name( 'y' ) ),
+                                Literal.new( :value(  1  ) ),
                             )
-                        ]
+                        )
+                    ),
+                    :if_true( Var.new( :name( 'x' ) ) ),
+                    :if_false(
+                        Apply.new(
+                            :name( '+' ),
+                            :args(
+                                Var.new( :name( 'x' ) ),
+                                Apply.new(
+                                    :name( 'mul' ),
+                                    :args(
+                                        Var.new( :name( 'x' ) ),
+                                        Apply.new(
+                                            :name( '-' ),
+                                            :args(
+                                                Var.new( :name( 'y' ) ),
+                                                Literal.new( :value(  1  ) ),
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
                     )
                 )
             )
-        ],
-        body => Apply.new(
-            name => 'mul',
-            args => [
-                Literal.new( value => 5 ),
-                Literal.new( value => 5 )
-            ]
         )
     ),
-    #Condition.new(
-    #    condition => Apply.new(
-    #        name => '==',
-    #        args => [
-    #            Literal.new( value => 2 ),
-    #            Literal.new( value => 3 ),
-    #        ]
-    #    ),
-    #    if_true  => Literal.new( value => 'TRUE!!!'  ),
-    #    if_false => Literal.new( value => 'FALSE!!!' ),
-    #), 
-    $root_env 
-;
+    :body(
+        Apply.new(
+            :name( 'mul' ),
+            :args(
+                Literal.new( :value( 5 ) ),
+                Literal.new( :value( 5 ) ),
+            )
+        )
+    )
+);
+
+say evaluate( $root_node, Scope.new( :env($root_env) ) ).perl;
 
 ## ---------------------------------------------
+
+#Cond.new(
+#    condition => Apply.new(
+#        :name( '==' ),
+#        :args(
+#            Literal.new( value => 2 ),
+#            Literal.new( value => 3 ),
+#        )
+#    ),
+#    if_true  => Literal.new( value => 'TRUE!!!'  ),
+#    if_false => Literal.new( value => 'FALSE!!!' ),
+#),
+
