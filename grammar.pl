9ab219020d1e29c1350bcbedccad8f2b7f41c261
Stevan Little <stevan.little@booking.com>
2015-03-06 12:27:54 +0100

    more stuff

--- grammar.pl
+++ grammar.pl
@@ -7,20 +7,96 @@ grammar PhP {
 
     rule statement {
         [
-        | <let-statement>  
+        | <let-statement> 
+        | <let-rec-statement> 
         ]
     }
 
+    # let blocks ...
+
     rule let-statement {
-        "let" <.ws>? <identifier> <.ws>? "=" <.ws>? <expression> <.ws>? "in" <.ws>? <expression> <.ws>? ";;"
+        "let" <.ws>? <let-value> <.ws>? "in" <.ws>? <let-body> <.ws>? ";;"
     }    
+
+    rule let-value {
+        <identifier> <.ws>? "=" <.ws>? <let-statement-value>
+    }
+
+    rule let-statement-value {
+        [
+        | <func-statement>  
+        | <expression>
+        ]
+    } 
+
+    rule let-body {
+        <expression>
+    } 
+
+    # let rec
+
+    rule let-rec-statement {
+        "let" <.ws>? "rec" <.ws>? <let-value-set> <.ws>? "in" <.ws>? <let-body> <.ws>? ";;"
+    }    
+
+    rule let-value-set {
+        <let-value> ("," <let-value-set>)?
+    }
+
+    # functions ...
+
+    rule func-statement {
+        "fun" <.ws>? "(" <func-param-list> ")" <.ws>? "\{" <.ws>? <func-body> <.ws>? "\}"
+    }    
+
+    rule func-param-list {
+        <func-param> ("," <func-param-list>)?
+    }
+
+    rule func-param {
+        <identifier>
+    }
+
+    rule func-body {
+        <expression>
+    }
+
+    # expressions ...
 
     rule expression {
         [    
+        | <cons-cell-expression>
+        | <apply-expression>
+        | <cond-expression>
         | <binary-expression>
         | <literal>        
-        | <identifier>              
+        | <identifier>  
+        | "(" <expression> ")"            
         ]
+    }
+
+    rule cons-cell-expression {
+        "[" <.ws>? <cons-cell-expression-head> <.ws>? <cons-cell-expression-tail>+ <.ws>? "]"
+    }
+
+    rule cons-cell-expression-head {
+        <expression>
+    }
+
+    rule cons-cell-expression-tail {
+        "::" <.ws>? <expression>
+    }
+
+    rule apply-expression {
+        <identifier> <.ws>? "(" <.ws>? <apply-argument-list> <.ws>? ")"
+    }
+
+    rule apply-argument-list {
+        <expression> ("," <apply-argument-list>)?
+    }
+
+    rule cond-expression {
+        "if" <.ws>? <expression> <.ws>? "then" <.ws>? <expression> <.ws>? "else" <.ws>? <expression>
     }
 
     rule binary-expression {
@@ -29,6 +105,8 @@ grammar PhP {
         | <identifier> <.ws>? <binary-op> <.ws>? <expression> 
         ]
     }
+
+    # tokens ....
 
     token binary-op {
         [
@@ -60,5 +138,5 @@ grammar PhP {
     token identifier-rest  { <[A..Za..z0..9_]> }
 }
 
-my $match = PhP.parse('let x = 2 + 2 in x + 3 * 10 / x ;;');
+my $match = PhP.parse('let rec bar = (20 + 2), foo = [ 10 :: 30 :: 40 ] in (if x == 10 then foo(20, 30, 2+2) else 30 - bar) ;;');
 say ~ $match.gist;
